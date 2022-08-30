4eae0318c90aa90900a41c963678833ead0a4925
Daniel Maya <60054217+danielmayav@users.noreply.github.com>
2022-08-29 21:28:19 -0500

    starting

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Figura:
+    def __init__(self, posicionX, posicionY):
+        self.posicionX = posicionX
+        self.posicionY = posicionY
+
+    def calcular_area():
+        pass
+
+    def calcular_perimetro():
+        pass
+    
